33f92f6027cb812c887d8a9d7b9ce8b1543adfa8
David Fialho <fialho.david@protonmail.com>
2017-09-16 01:21:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TVeeBot Communication
+Set of packages and modules to enable communication between clients and daemons
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tveebot-communication',
+    version='0.1',
+    description='Set of packages and modules to enable communication '
+                'between clients and daemons',
+    url='https://github.com/tveebot/communication',
+    license='MIT',
+    author='David Fialho',
+    author_email='fialho.david@protonmail.com',
+
+    packages=find_packages(),
+)
